c586c1c650c3a5d2d6a68e6a5cda00fe87c2abab
m-wayne <mwayne@scribd.com>
2024-03-26 10:43:04 -0700

    allow for array input of context

--- index.ts
+++ index.ts
@@ -9,21 +9,23 @@ const getRequiredInput = (name: string): string =>
   const sha = core.getInput('sha') || github.context.sha
   const state = getRequiredInput('state') as StatusState
   const description = core.getInput('description')
-  const context = getRequiredInput('context')
+  const contextInput = getRequiredInput('context')
+  const context = contextInput.trim().split('\n');
   const targetUrl = core.getInput('target_url')
   const githubToken = core.getInput('github_token')
-
   const octokit = new github.GitHub(githubToken)
-  await octokit.repos.createStatus({
-    ...github.context.repo,
-    sha,
-    state,
-    description,
-    context,
-    ...(targetUrl && { target_url: targetUrl }),
-  })
 
-  console.log('Successfully posted a GitHub commit status.')
+  for (const contextElement of context) {
+    await octokit.repos.createStatus({
+      ...github.context.repo,
+      sha,
+      state,
+      description,
+      context: contextElement,
+      ...(targetUrl && { target_url: targetUrl })
+    })
+    console.log(`Successfully posted a GitHub commit status for ${contextElement}.`)
+}
 })().catch(error => {
   console.error(error)
   core.setFailed(error.message)
